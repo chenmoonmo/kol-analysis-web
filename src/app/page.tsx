6c389ef088b62a5330d764bb1b8c2b1761248ec6
chenmoonmo <chenmoonmo@gmail.com>
2024-11-21 19:07:20 +0800

    feat: update followers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,6 +36,16 @@ export default async function Home({
         "10_rate": number;
         "15_rate": number;
         "30_rate": number;
+        "kol": {
+          name: string;
+          favourites_count: number;
+          followers_count: number;
+          friends_count: number;
+          verified: boolean;
+          rest_id: string;
+          avatar: string;
+          description: string;
+        }
       }[];
     }
   ).data;
@@ -47,6 +57,8 @@ export default async function Home({
         <Table.Header>
           <Table.Row>
             <Table.ColumnHeaderCell>KOL name</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>followers</Table.ColumnHeaderCell>
+
             <Table.ColumnHeaderCell>
               <Link
                 href={`?level=${level}&key=tweet_count`}
@@ -64,8 +76,8 @@ export default async function Home({
                     <path
                       d="M7.14645 2.14645C7.34171 1.95118 7.65829 1.95118 7.85355 2.14645L11.8536 6.14645C12.0488 6.34171 12.0488 6.65829 11.8536 6.85355C11.6583 7.04882 11.3417 7.04882 11.1464 6.85355L8 3.70711L8 12.5C8 12.7761 7.77614 13 7.5 13C7.22386 13 7 12.7761 7 12.5L7 3.70711L3.85355 6.85355C3.65829 7.04882 3.34171 7.04882 3.14645 6.85355C2.95118 6.65829 2.95118 6.34171 3.14645 6.14645L7.14645 2.14645Z"
                       fill="currentColor"
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
+                      fillRule="evenodd"
+                      clipRule="evenodd"
                     ></path>
                   </svg>
                 )}
@@ -94,8 +106,8 @@ export default async function Home({
                     <path
                       d="M7.14645 2.14645C7.34171 1.95118 7.65829 1.95118 7.85355 2.14645L11.8536 6.14645C12.0488 6.34171 12.0488 6.65829 11.8536 6.85355C11.6583 7.04882 11.3417 7.04882 11.1464 6.85355L8 3.70711L8 12.5C8 12.7761 7.77614 13 7.5 13C7.22386 13 7 12.7761 7 12.5L7 3.70711L3.85355 6.85355C3.65829 7.04882 3.34171 7.04882 3.14645 6.85355C2.95118 6.65829 2.95118 6.34171 3.14645 6.14645L7.14645 2.14645Z"
                       fill="currentColor"
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
+                      fillRule="evenodd"
+                      clipRule="evenodd"
                     ></path>
                   </svg>
                 )}
@@ -118,8 +130,8 @@ export default async function Home({
                     <path
                       d="M7.14645 2.14645C7.34171 1.95118 7.65829 1.95118 7.85355 2.14645L11.8536 6.14645C12.0488 6.34171 12.0488 6.65829 11.8536 6.85355C11.6583 7.04882 11.3417 7.04882 11.1464 6.85355L8 3.70711L8 12.5C8 12.7761 7.77614 13 7.5 13C7.22386 13 7 12.7761 7 12.5L7 3.70711L3.85355 6.85355C3.65829 7.04882 3.34171 7.04882 3.14645 6.85355C2.95118 6.65829 2.95118 6.34171 3.14645 6.14645L7.14645 2.14645Z"
                       fill="currentColor"
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
+                      fillRule="evenodd"
+                      clipRule="evenodd"
                     ></path>
                   </svg>
                 )}
@@ -142,8 +154,8 @@ export default async function Home({
                     <path
                       d="M7.14645 2.14645C7.34171 1.95118 7.65829 1.95118 7.85355 2.14645L11.8536 6.14645C12.0488 6.34171 12.0488 6.65829 11.8536 6.85355C11.6583 7.04882 11.3417 7.04882 11.1464 6.85355L8 3.70711L8 12.5C8 12.7761 7.77614 13 7.5 13C7.22386 13 7 12.7761 7 12.5L7 3.70711L3.85355 6.85355C3.65829 7.04882 3.34171 7.04882 3.14645 6.85355C2.95118 6.65829 2.95118 6.34171 3.14645 6.14645L7.14645 2.14645Z"
                       fill="currentColor"
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
+                      fillRule="evenodd"
+                      clipRule="evenodd"
                     ></path>
                   </svg>
                 )}
@@ -163,6 +175,7 @@ export default async function Home({
                   @{item.screen_name}
                 </Link>
               </Table.Cell>
+              <Table.Cell>{item.kol.followers_count}</Table.Cell>
               <Table.Cell>{item.tweet_count}</Table.Cell>
               <Table.Cell>{item["10_win_count"]}</Table.Cell>
               <Table.Cell>{item["10_lose_count"]}</Table.Cell>
